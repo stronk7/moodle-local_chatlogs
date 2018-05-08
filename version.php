26f5f32580224378d5c76f9514fcc9995835acbe
David Mudrák <david@moodle.com>
2018-05-08 22:23:19 +0200

    Changing the version numbering scheme, no need for regular updates

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->component = 'local_chatlogs';
-$plugin->release = '3.3.0';
+$plugin->release = '2017.2';
 $plugin->version = 2017050900;
 $plugin->requires = 2017050900;
 $plugin->maturity = MATURITY_STABLE;
